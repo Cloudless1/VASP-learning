4fa8132e0549ffdccf27ee9603197f105e27a65d
Cloudless1 <89248088+Cloudless1@users.noreply.github.com>
2026-03-18 16:54:11 +0800

    Delete CHECK-geo-finish.sh

--- CHECK-geo-finish.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#this scripts is used to check the vasp task converges normally and ends
-for i in * ; do
-echo $i
-grep 'reached required accuracy - stopping structural energy minimisation' $i/OUTCAR
-done
