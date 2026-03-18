40f3f2618b1944f06a52272d2d5e6a5a8337ac5c
Cloudless1 <89248088+Cloudless1@users.noreply.github.com>
2026-03-18 16:53:49 +0800

    Delete BADER-calculate.sh

--- BADER-calculate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#this scripts is uesd to calculated Bader charge
-
-for i in *;do 
-cd $i
-chgsum.pl AECCAR0 AECCAR2  > bader1.log
-bader CHGCAR -ref CHGCAR_sum  > bader2.log
-cd ../;
-done
